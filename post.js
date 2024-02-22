5f16492c8593ac885ba47bef8ac1ed1a6b4f311c
prnamhr <parnamehri@gmail.com>
2024-02-22 21:42:31 +0330

    post page with comments

--- post.js
+++ post.js
@@ -26,8 +26,10 @@ router.get('/:postId', async (req, res) => {
 
     try {
         const post = await sql`
-            SELECT * FROM posts
-            WHERE post_id = ${postId}`;
+            SELECT * FROM 
+            posts
+            INNER JOIN users ON posts.user_Id = users.user_Id
+            WHERE posts.post_Id = ${postId}`;
 
         if (post.length === 0) {
             return res.status(404).json({ error: 'Post not found' });
@@ -39,5 +41,41 @@ router.get('/:postId', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+router.post('/:username/:postId/comment', async (req, res) => {
+    const { username,postId } = req.params;
+    const { comment_text } = req.body;
+    console.log(comment_text,postId,username)
+
+    try {
+        const result = await sql`
+            INSERT INTO comments (post_id, user_id, comment_text)
+            VALUES (${postId}, (SELECT user_id FROM users WHERE username = ${username}), ${comment_text})
+            RETURNING *`;
+
+        res.status(201).json(result[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+router.get('/:postId/comments', async (req, res) => {
+    const { postId } = req.params;
+
+    try {
+        const comments = await sql`
+            SELECT * FROM comments
+            INNER JOIN users ON comments.user_Id = users.user_Id
+            WHERE post_id = ${postId}`;
+        console.log(comments)
+        res.json(comments);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+
 
 module.exports = router;
